5424997946dcfde52bc2c503c49b7f8439123261
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2024-12-10 16:02:42 -0600

    Time: 3 ms (85.71%), Space: 17.5 MB (5.37%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -1,10 +1,11 @@
+import math
+
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        if num < 5:
-            return False
-        result = 1
-        for i in range(2, int(math.sqrt(num)+1)):
-            if (num % i == 0):
-                result += i + num // i #get integer and its divisor
-        print(result)
-        return result == num
+        if num == 1: return False
+        res = 1
+        for i in range(2, math.ceil(num**0.5)):
+            if num % i == 0:
+                print(i, num // i, res)
+                res += i + num // i            
+        return res == num
