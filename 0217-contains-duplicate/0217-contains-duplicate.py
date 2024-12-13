677361405186a3d3f808660b6f95895f9d7a995e
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2024-12-13 16:26:30 -0600

    Time: 15 ms (28.89%), Space: 31.1 MB (40.48%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,7 +1,10 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # Brute: iterate through nested loop and determine if each value has a duplicate O(n^2)
+        # Better: sort the array and see if any value has a duplicate O(nlogn)
+        # Optimal: O(n) O(n) store values in set and return true if it exists in the set
         store = set()
-        for  i in range(0, len(nums)):
+        for i in range(0, len(nums)):
             if nums[i] in store:
                 return True
             else:
