7976570f8ce2de77405e76a019bb9e05ea7327d9
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2024-12-13 15:42:43 -0600

    Time: 0 ms (100%), Space: 17.5 MB (5.8%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -1,7 +1,13 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        switch = 0
-        for i in range(0, len(nums)):
-            if nums[i] < nums[i-1]:
-                switch += 1
-        return switch <= 1
+        # O(n^2) check each index and see if it is constantly incr from there
+        # O(nlogn) sort array and then see if it is const increasing
+        # see if there is one or 0 decrement - if so return true, otherwise return false
+
+        result = 0
+        if nums[0] < nums[-1]:
+            result += 1
+        for i in range(0, len(nums) - 1):
+            if nums[i + 1] < nums[i]:
+                result += 1
+        return True if result < 2 else False
