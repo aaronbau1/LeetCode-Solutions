4a6e88a1175cd1bc0ffa46e8888665e97001053e
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2024-12-16 15:15:29 -0600

    Time: 0 ms (100%), Space: 18.1 MB (28.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,16 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # Brute: copy all non-zeros to an array then copy back over
+        # Optimal: two pointer approach where we keep a non-zero and zero pointer and when we reach a non-zero, we swap
         zero_index = 0
-        for i in range(len(nums)):
-            if i == 0:
+
+        # [0,0,1,0,3,12]
+        # [1,1,0,0,0,3,12]
+        for i in range(0, len(nums)):
+            if nums[i] == 0:
                 zero_index = i
                 break
-
+        
         for i in range(zero_index, len(nums)):
             if nums[i] != 0:
                 nums[i], nums[zero_index] = nums[zero_index], nums[i]
                 zero_index += 1
-        print(nums)
-        
-        #[1, 0, 0, 3, 4]
