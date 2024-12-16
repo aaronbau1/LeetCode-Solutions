097a192eaddadc32c84c40fffa5ceadbe0388bde
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2024-12-16 13:45:32 -0600

    Time: 2 ms (55.64%), Space: 18.3 MB (24.31%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,10 +1,25 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        uniq_index = 1
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
-                continue
-            else:
-                nums[uniq_index] = nums[i]
-                uniq_index += 1
-        return uniq_index
+        # brute: for each element, count the number of duplicated instances and store them
+            # in the front of the array O(n^2)
+        # optimal: use two pointers to count the number of duplicates as you progress through the array
+            # when it switches to a new element, modify the array in place for each unique element
+
+        # [0,0,1,1,1,2,2,3,3,4]
+        result = 1
+        unique_num_index = 0
+        current_num = nums[0]
+        for i in range(0, len(nums)):
+            if nums[i] != current_num:
+                nums[unique_num_index] = current_num
+                unique_num_index += 1
+                result += 1
+                current_num = nums[i]
+                # Captures end number and adds to array if it is unique
+                if i == len(nums) - 1:
+                    nums[unique_num_index] = current_num
+            if i == len(nums) - 1:
+                    nums[unique_num_index] = current_num
+        return result
+        
+            
