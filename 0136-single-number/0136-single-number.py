050a097e93fe915dcb2ec9424b06ac4ff91acc99
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2024-12-17 11:43:58 -0600

    Time: 7 ms (35.89%), Space: 19.6 MB (10.32%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result = nums[0]
-        for i in range(1, len(nums)):
+        # Brute
+        # Move all into a hash array, then check to see which value only has 1 O(n) O(n)
+        # Optimal
+        # Use XOR operator and iterate through O(n) since x^x = 0
+        result = 0
+        for i in range(0,len(nums)):
             result = result ^ nums[i]
         return result
