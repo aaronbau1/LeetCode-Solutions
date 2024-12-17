da4eadcb5f81b7df823a10ba2f19172e0a506430
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2024-12-17 10:21:20 -0600

    Time: 0 ms (100%), Space: 18.1 MB (8.74%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,15 +1,20 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # Hashset O(2n) O(n)
+        # Brute: For each value, check if it exists in second array - if unique add to result array O(n^2)
+        # Optimal: Create a set of the first array, then iterate through next array 
+            # and add unique elements, then convert from set to result array O(3n) O(n)
+        
         store1 = set()
         store2 = set()
         result = []
-        for i in range(0, len(nums1)):
-            if not nums1[i] in store1:
-                store1.add(nums1[i])
-        for i in range(0, len(nums2)):
-            if  nums2[i] in store1 and nums2[i] not in store2:
-                store2.add(nums2[i])
+
+        for val in nums1:
+            store1.add(val)
+        
+        for val in nums2:
+            if (val in store1) and (val not in store2):
+                store2.add(val)
+
         for val in store2:
             result.append(val)
         return result
