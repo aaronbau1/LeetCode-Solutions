2ced98f94ec7f0883ba543c7d1b99d4597cfc724
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2024-12-17 10:52:14 -0600

    Time: 3 ms (64.78%), Space: 19 MB (5.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # Brute
+        # iterate through 0-n and see if value is included in the list using "val in nums" O(n^2)
+        # Optimal
+        # Use XOR operator O(n) 
         result = len(nums)
+        # 3
+        # O ^ 1 ^ 2
+        # 3 ^ 0 ^ 1
         for i in range(0, len(nums)):
-            result = result ^ i ^ nums[i]
+            result ^= i ^ nums[i]
         return result
