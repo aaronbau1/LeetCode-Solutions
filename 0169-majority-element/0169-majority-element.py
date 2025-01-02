91e5639c50ba4d032d8046d35857e091acb9c2c4
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-02 15:22:47 -0600

    Time: 3 ms (82.99%), Space: 19.4 MB (5.3%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,18 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Brute: Check each element count in nested loop O(n^2)
-        # Better: use hashmap and count freq, if greater than n/2 then return. O(n) O(n)
-        # 
+        # brute - iterate for each value and count number of times it appears in array O(n^2)
+        # better - use hashmap and store frequencies, then iterate through and return the greatest O(n) O(n)
+        # optimal - polling algorithm, iterate through and choose candidate and track local count
 
-        count = 0
-        cand_el = nums[0]
-        for i in range(len(nums)):
-            if cand_el != nums[i]:
-                count -= 1
-                if count < 0:
-                    cand_el = nums[i]
-                    count = 0
-            else:
+        count = 1
+        cand = nums[0]
+        for i in range(1, len(nums)):
+            if count == 0:
+                cand = nums[i]
+                count = 1
+            elif nums[i] == cand:
                 count += 1
-        return cand_el
-            
+            else:
+                count -= 1
+        return cand
