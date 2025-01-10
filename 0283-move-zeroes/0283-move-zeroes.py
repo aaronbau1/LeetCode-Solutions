1d4cfb3b4279b0d6ccf1588557f61f220959bf3c
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-10 10:33:04 -0600

    Time: 6 ms (51.38%), Space: 19 MB (11.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,18 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Brute: copy all non-zeros to an array then copy back over
-        # Optimal: two pointer approach where we keep a non-zero and zero pointer and when we reach a non-zero, we swap
-        zero_index = 0
-
-        # [0,0,1,0,3,12]
-        # [1,1,0,0,0,3,12]
-        for i in range(0, len(nums)):
-            if nums[i] == 0:
-                zero_index = i
-                break
+        # bruter: remove zeros from list and keep count, then add them all in at the end
+        # optimal: two pointer, if there is a zero then swap with non-zero element
+        #  [0,0,1,0,1,3,12]
         
-        for i in range(zero_index, len(nums)):
+        # find first non-zero index and first zero index (0,2)
+        # swap [1,0,0,0,1,3,12], increment zero index
+        # 
+        # iterate from the first non-zero index
+        # go until you find a non-zero
+        # swap
+        # increment zero index by one
+        # go until end of array
+        zero_index = 0
+        while zero_index < len(nums) and nums[zero_index] != 0:
+            zero_index += 1
+        i = zero_index
+        while i < len(nums):
             if nums[i] != 0:
                 nums[i], nums[zero_index] = nums[zero_index], nums[i]
                 zero_index += 1
+            i += 1
+        
