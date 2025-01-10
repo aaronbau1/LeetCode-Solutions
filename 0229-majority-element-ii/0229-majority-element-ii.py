2500eceb31cd5224cf6066b5bb8f133cc97739fc
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-09 18:30:49 -0600

    Time: 6 ms (38.94%), Space: 19.2 MB (6.54%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -2,18 +2,36 @@ import math
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        # brute: check all numbers and frequencies O(n^3) O(n)
-        # better: 
-        # optimal: 
-        result = []
-        store = {}
+        count1 = 0
+        count2 = 0        
+        elmt1 = float("-inf")
+        elmt2 = float("inf")
+
         for i in range(len(nums)):
-            if nums[i] in store:
-                store[nums[i]] += 1
+            if count1 == 0 and nums[i] != elmt2:
+                elmt1 = nums[i]
+                count1 = 1
+            elif count2 == 0 and nums[i] != elmt1:
+                elmt2 = nums[i]
+                count2 = 1
+            elif nums[i] == elmt1:
+                count1 += 1
+            elif nums[i] == elmt2:
+                count2 += 1
             else:
-                store[nums[i]] = 1
-        print(store)
-        for val in store:
-            if store[val] > math.floor(len(nums) // 3):
-                result.append(val)
+                count1 -= 1
+                count2 -= 1
+        cnt1 = 0
+        cnt2 = 0
+        for i in range(len(nums)):
+            if nums[i] == elmt1:
+                cnt1 += 1
+            elif nums[i] == elmt2:
+                cnt2 += 1
+        print(cnt1, cnt2)
+        result = []
+        if cnt1 >= len(nums) // 3 + 1:
+            result.append(elmt1)
+        if cnt2 >= len(nums) // 3 + 1:
+            result.append(elmt2)
         return result
