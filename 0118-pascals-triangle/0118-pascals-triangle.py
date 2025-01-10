d33cae0812af23963bf019712878f247d45f807a
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-10 11:06:57 -0600

    Time: 0 ms (100%), Space: 17.9 MB (14.65%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,12 +1,17 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        # brute: go line by line and print value based on the two previous values O(n^2)
+        # optimal know that nCr is used, so create each row based on previous value
+        # n! / (r)! * (n-r)! derives to previous value * (row - col) / col
+        # row 5
+        # [1,4,6,4,1]: 4 C 0, 4 C 1, 4 C 2, 4 C 3, 4 C 4
         result = []
-
-        for row_index in range(1, numRows + 1):
-            row = [1]
+        for i in range(1, numRows+1):
             val = 1
-            for col_index in range(1, row_index):
-                val = val * (row_index - col_index) // col_index
+            row = [1]
+            # avoid dividng by zero, start with 1
+            for j in range(1, i):
+                val = int(val * (i - j)/j)
                 row.append(val)
             result.append(row)
         return result
