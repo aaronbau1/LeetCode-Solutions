e7fb4f707b2105d9be7f6cd00f88b3215e31dde7
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-10 13:34:43 -0600

    Time: 26 ms (86.72%), Space: 20.3 MB (35.58%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,22 +1,24 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # brute: check every subarray and sum them, see which equal k - O(n^3)
-        # better: 
-        # optimal: prefix sum - store sum at each point and check if the remainder
-                # is within a store. If it is, increment counter
-
+        # brute: sum all possible array O(n^3)
+        # better: calculate sum while creating all possible arrays O(n^2)
+        # optimal: prefix sum, but integrate a count variable to account for 
+        # multiple prefix sums with same value
+        # note, must start at 0 to account for subarrays including first value
         count = 0
         psum = 0
         store = {0:1}
-
+        # [1,2,3]
         for i in range(len(nums)):
             psum += nums[i]
             rem = psum - k
+
             if rem in store:
                 count += store[rem]
+            
             if psum in store:
                 store[psum] += 1
             else:
                 store[psum] = 1
-            
         return count
+            
