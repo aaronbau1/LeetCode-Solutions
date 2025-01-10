7dca4a2ac9bd201441642f5729f6f58f9af4ac24
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-09 23:54:59 -0600

    Time: 7 ms (46.79%), Space: 25.6 MB (19.53%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,16 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)  # Handle cases where k > len(nums)
+        k = k % len(nums)
+        n = len(nums)
 
-        # Reverse the last k elements
-        nums[:len(nums) - k] = nums[:len(nums) - k][::-1]
-        # print(nums)  # Debug output
-
-        # Reverse the rest of the array
-        nums[len(nums) - k:] = nums[len(nums) - k:][::-1]
-        # print(nums)  # Debug output
-
-        # Reverse the entire array
-        nums[:] = nums[::-1]
-        # print(nums)  # Debug output
+        nums[n-k:] = nums[n-k:][::-1]
+        nums[:n-k] = nums[:n-k][::-1]
+        nums[:] = nums[:][::-1]
