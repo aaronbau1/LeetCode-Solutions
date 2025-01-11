25f5f8d3bef05e767cb6674926e7a6eea0fc16c8
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-11 13:22:31 -0600

    Time: 11 ms (34.12%), Space: 18.5 MB (37.55%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,17 +1,17 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        # brute - iterate through and select each index, then sum on left and right side O(n^2)
-        # better - use a hashmap and record sum for right iteration, then iterate left and see where it matches O(n) O(n)
-        # optimal - sum all values, and then iterate once through and compare right and left
         total = 0
         for i in range(len(nums)):
             total += nums[i]
-
-        left = 0
+        
+        index = 0
         right = total
-        for i in range(len(nums)):
-            right -= nums[i]
+        left = 0
+        while (index < len(nums)):
+            left += nums[index]
             if left == right:
-                return i
-            left += nums[i]
+                return index
+            right -= nums[index]
+            index += 1
         return -1
+        
