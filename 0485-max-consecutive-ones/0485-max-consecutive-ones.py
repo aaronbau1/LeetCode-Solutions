a67f0833f5b5d342550ee622c1f850ceeadcea23
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-11 10:00:33 -0600

    Time: 19 ms (60.04%), Space: 20.2 MB (10.48%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -1,17 +1,15 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        # Brute: 
-        # Optimal: iterate through and keep track of local max / global max O(n)
+        # optimal: count local and max value of contiguous integers
         local_count = 0
-        max_count = 0
+        arr_max = 0
 
-        for i in range(0, len(nums)):
+        for i in range(len(nums)):
             if nums[i] == 0:
-                if local_count > max_count:
-                    max_count = local_count
+                arr_max = max(local_count, arr_max)
                 local_count = 0
             else:
                 local_count += 1
-                if i == len(nums) - 1 and local_count > max_count:
-                    max_count = local_count
-        return max_count
+        arr_max = max(local_count, arr_max)
+        return arr_max
+            
