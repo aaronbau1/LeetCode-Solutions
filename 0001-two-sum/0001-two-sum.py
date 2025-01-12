8ec72ece61d3cdf42e6b4f6205262e04de5c6c79
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-12 12:18:10 -0600

    Time: 0 ms (100%), Space: 18.9 MB (23.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
         store = {}
         for i in range(len(nums)):
             rem = target - nums[i]
+
             if rem in store:
-                return [i, store[rem]]
+                return [store[rem], i]
             else:
                 store[nums[i]] = i
