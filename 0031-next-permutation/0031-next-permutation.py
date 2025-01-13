15ac2eb49772040b16e7dfc06aff9e66e0eef1ea
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-13 09:32:34 -0600

    Time: 0 ms (100%), Space: 17.9 MB (5.98%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,20 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # [1,2,5,4,3,0,0] -> [ 1,3,0,0,2,4,5]
         pivot = -1
-        for i in range(len(nums)-1,0,-1):
-            if nums[i] > nums[i-1]:
-                pivot = i-1
+        for i in range(len(nums)-1, 0, -1):
+            if nums[i-1] < nums[i]:
+                pivot = i - 1
                 break
-
+        # print(pivot)
         if pivot == -1:
             nums[:] = nums[::-1]
             return
-        
+
         for i in range(len(nums)-1, pivot, -1):
             if nums[i] > nums[pivot]:
                 nums[i], nums[pivot] = nums[pivot], nums[i]
                 break
-        
+        # print(nums)
         nums[pivot+1:] = nums[pivot+1:][::-1]
+
+
+
+        
