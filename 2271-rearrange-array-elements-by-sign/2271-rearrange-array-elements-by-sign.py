344f4f67366c92efed6aeb9f1ad905cee508fc92
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-13 08:55:25 -0600

    Time: 43 ms (73.93%), Space: 42.1 MB (51.64%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -1,16 +1,13 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        # brute - use array and fill in values based on even and odd index O(n) O(n)
-        # two pointers and iterate through, then swap values if the 
-        # 
         result = [0] * len(nums)
-        pos = 0
-        neg = 1
+        pos_index = 0
+        neg_index = 1
         for i in range(len(nums)):
             if nums[i] > 0:
-                result[pos] = nums[i]
-                pos += 2
+                result[pos_index] = nums[i]
+                pos_index += 2
             else:
-                result[neg] = nums[i]
-                neg += 2
+                result[neg_index] = nums[i]
+                neg_index += 2
         return result
