78e075479b697f1817be3f6a36aa3b56caa4e092
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-14 10:48:55 -0600

    Time: 5 ms (44.17%), Space: 19.1 MB (20.51%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -2,43 +2,55 @@ import math
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        # brute checking each element and counting the frequency. If they are greater than floor(n/3), put into array
+            # O(n^2)
+        # better using a hashmap to count the frequency, converting it to O(n) O(n) solution
+        # optimal using two candidates and then manually confirming the candidates after O(2n)
+
+        # declare two count variables
+        # iterate through the loop from 0 -> n:
+            # if the count is zero for first and != 2nd elmt
+                # set count to 1 and change variable to current
+            # elif the count is zero for the second and != 1st elmt
+                # set count to 1 and change variable to current
+            # elif elmt equal to 1st elemt
+                # increment count 1
+            # elif elmt equal to 2nd elmt
+                # increment count 2
         
-    # brute: check each element and see if count is greater than floor(n/3) O(n^2)
-    # better: use hashmap and count freq - 
-        #   if element is greater than floor(n/3) then add to array. break when two values
-    # optimal: use two pointers and two frequency checks. get two candidates. Then manually check those two candidates
-    # [3,2,3,0,0,3,2]
+        # manually run through and compare values
+
         count1 = 0
         count2 = 0
-        elmt1 = 0
-        elmt2 = 0
+        cand1 = float("-inf")
+        cand2 = float("-inf")
 
         for i in range(len(nums)):
-            if count1 == 0 and elmt2 != nums[i]:
-                elmt1 = nums[i]
+            if count1 == 0 and nums[i] != cand2:
+                cand1 = nums[i]
                 count1 = 1
-            elif count2 == 0 and elmt1 != nums[i]:
-                elmt2 = nums[i]
+            elif count2 == 0 and nums[i] != cand1:
+                cand2 = nums[i]
                 count2 = 1
-            elif nums[i] == elmt1:
+            elif nums[i] == cand1:
                 count1 += 1
-            elif nums[i] == elmt2:
+            elif nums[i] == cand2:
                 count2 += 1
             else:
                 count1 -= 1
                 count2 -= 1
         
-        # manual check
+        check = len(nums) // 3 + 1
         cnt1 = 0
         cnt2 = 0
         for i in range(len(nums)):
-            if nums[i] == elmt1:
+            if nums[i] == cand1:
                 cnt1 += 1
-            elif nums[i] == elmt2:
+            elif nums[i] == cand2:
                 cnt2 += 1
         result = []
-        if cnt1 > math.floor(len(nums)//3):
-            result.append(elmt1)
-        if cnt2 > math.floor(len(nums)//3):
-            result.append(elmt2)
+        if cnt1 >= check:
+            result.append(cand1)
+        if cnt2 >= check:
+            result.append(cand2)
         return result
