09e1cbed5b09e047d28739a90438bb519b969fc4
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-14 12:10:08 -0600

    Time: 5 ms (30.96%), Space: 18.2 MB (54.87%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,23 +3,33 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        row_len = len(matrix)
-        col_len = len(matrix[0])
-        rows = [0] * row_len
-        cols = [0] * col_len
+        # brute - use a 2nd array and make change in there based on zeros
+        # better - use a result array that signals the zeros, then update the given array based on that
+        # optimal - use the 1st row / col of the matrix as the signal array + one variable
+        first_col = 1
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        for row in range(rows):
+            for col in range(cols):
+                if matrix[row][col] == 0:
+                    matrix[row][0] = 0
+
+                    if col != 0:
+                        matrix[0][col] = 0
+                    else:
+                        first_col = 0
+
+        for row in range(1, rows):
+            for col in range(1, cols):
+                if matrix[row][col] != 0:
+                    if matrix[row][0] == 0 or matrix[0][col] == 0:
+                        matrix[row][col] = 0
         
-        for i in range(row_len):
-            for j in range(col_len):
-                if matrix[i][j] == 0:
-                    rows[i] = 1
-                    cols[j] = 1
-        
-        for i in range(row_len):
-            if rows[i] == 1:
-                for j in range(col_len):
-                    matrix[i][j] = 0
-        
-        for j in range(col_len):
-            if cols[j] == 1:
-                for i in range(row_len):
-                    matrix[i][j] = 0
+        if matrix[0][0] == 0:
+            for col in range(cols):
+                matrix[0][col] = 0
+
+        if first_col == 0:
+            for row in range(rows):
+                matrix[row][0] = 0
