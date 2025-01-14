25bb3342ed96bc102b7b6d504c3afc5d902cfa06
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-14 10:11:08 -0600

    Time: 564 ms (65.24%), Space: 20.6 MB (41.3%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        # brute find all combinations of 3 numbers and sum them and sort them. O(n^3)
+        # better use a storage set to store combinations, know that k = -i + -j so we can reduce to two loops
+        # optimal use a fixed pointer that we loop through with a two pointer approach 
         nums.sort()
+        result = []
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
             j = i + 1
             k = len(nums) - 1
             while j < k:
-                local_sum = nums[i] + nums[j] + nums[k]
-                if local_sum < 0:
-                    j += 1
-                    # while nums[j] == nums[j-1]:
-                    #     j += 1
-                elif local_sum > 0:
+                threeSum = nums[i] + nums[j] + nums[k]
+                if threeSum > 0:
                     k -= 1
-                    # while nums[k] == nums[k+1]:
-                    #     k -= 1
+                elif threeSum < 0:
+                    j += 1
                 else:
                     result.append([nums[i], nums[j], nums[k]])
                     j += 1
-                    while nums[j] == nums[j-1] and j < k:
-                        j += 1 
+                    k -= 1
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k+1]:
+                        k -= 1
         return result
