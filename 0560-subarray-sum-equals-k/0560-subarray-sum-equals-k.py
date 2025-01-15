26c389018f5d4ee5e9e4347b0ceb1e3f5cd982c2
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-15 11:40:35 -0600

    Time: 24 ms (91.01%), Space: 20.3 MB (40.14%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,8 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # brute: find all subarrays and sum them O(n^3)
-        # better: sum subbarrays and you find them O(n^2)
-        # optimal: prefix sum
+        # brute find all subarrays and sum them, O(n^3) (two loops, and one loop to sum)
+        # better sum them as you move the pointer, O(n^2)
+        # optimal: Use prefix sum and store values as you progress O(n) O(n)
+        
+        # create a store, count, psum
+        # iterate with a prefix sum
+        # calculate the remainder
+        # if the remainder is in the store, incremenet the count
+        # if the psum is in the store, increment the store[psum]
+        # otherwise add the psum to the store
 
         count = 0
         psum = 0
@@ -20,3 +27,4 @@ class Solution:
             else:
                 store[psum] = 1
         return count
+
