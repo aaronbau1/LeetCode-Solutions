fb825b872e9dad1035f1818b35e2e75b92d65a73
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-15 12:10:55 -0600

    Time: 7 ms (23.57%), Space: 18.4 MB (29.36%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,12 +3,13 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # brute - use a 2nd array and make change in there based on zeros
-        # better - use a result array that signals the zeros, then update the given array based on that
-        # optimal - use the 1st row / col of the matrix as the signal array + one variable
-        first_col = 1
+        # brute: make a copy of the array - when an element is zero, mark the rows int he copy O(n^2) O(n)
+        # better: use an external row and column array and mark when an element is zero. Then iterate through and mark the array
+        # optimal: Use the first row and columns + one variable to mark array
+
         rows = len(matrix)
         cols = len(matrix[0])
+        first_col = False
 
         for row in range(rows):
             for col in range(cols):
@@ -18,18 +19,17 @@ class Solution:
                     if col != 0:
                         matrix[0][col] = 0
                     else:
-                        first_col = 0
-
+                        first_col = True
+        
         for row in range(1, rows):
             for col in range(1, cols):
-                if matrix[row][col] != 0:
-                    if matrix[row][0] == 0 or matrix[0][col] == 0:
-                        matrix[row][col] = 0
+                if matrix[row][0] == 0 or matrix[0][col] == 0:
+                    matrix[row][col] = 0
         
         if matrix[0][0] == 0:
             for col in range(cols):
                 matrix[0][col] = 0
-
-        if first_col == 0:
+        if first_col:
             for row in range(rows):
                 matrix[row][0] = 0
+        
