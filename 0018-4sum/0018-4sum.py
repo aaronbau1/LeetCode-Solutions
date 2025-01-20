6c4e855befa2f3ffda589b41795f19b89afbf5ee
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-20 16:34:19 -0600

    Time: 378 ms (68.29%), Space: 18.1 MB (11.8%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,13 +1,8 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        # brute: four loops, calculate all subarrays, sort them and add to a set to check if they exist
-        # better: elimite fourth loop by using target - i - j - k and using hashset for last value - then record all values as you iterate and if you find
-        # a result, then you can add it to the hashset for quad sets. O(n^3) O(2n)
-        # optimal 2 static, 2 pointers
-
         result = []
-        nums.sort()
         n = len(nums)
+        nums.sort()
         for a in range(n):
             if a > 0 and nums[a] == nums[a-1]:
                 continue
@@ -16,6 +11,7 @@ class Solution:
                     continue
                 c = b + 1
                 d = n - 1
+
                 while c < d:
                     fourSum = nums[a] + nums[b] + nums[c] + nums[d]
                     if fourSum > target:
@@ -24,11 +20,10 @@ class Solution:
                         c += 1
                     else:
                         result.append([nums[a], nums[b], nums[c], nums[d]])
-                        d -= 1
                         c += 1
+                        d -= 1
                         while c < d and nums[c] == nums[c-1]:
                             c += 1
                         while c < d and nums[d] == nums[d+1]:
                             d -= 1
         return result
-            
