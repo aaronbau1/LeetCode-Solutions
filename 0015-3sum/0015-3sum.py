d2c716e61cd752ba0869d74fea753ed36bb1b64e
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-26 13:10:59 -0600

    Time: 2942 ms (5.01%), Space: 20.8 MB (32.46%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,31 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # brute: calculate all subarrays that equal zero, sort, and store into a set. 
-        # better: know that k = - i - j, use set to find target value and for sums and another set to store triplets
-        # optimal
-        
-        result = []
-        nums.sort()
-
+        tripStore = set()
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            
-            j = i + 1
-            k = len(nums) - 1
-
-            while j < k:
-                threeSum = nums[i] + nums[j] + nums[k]
-                if threeSum > 0:
-                    k -= 1
-                elif threeSum < 0:
-                    j += 1
+            psum_store = set()
+            for j in range(i + 1, len(nums)):
+                target = -(nums[i] + nums[j])
+                if target in psum_store:
+                    temp = [nums[i], nums[j], target]
+                    temp.sort()
+                    tripStore.add(tuple(temp))
                 else:
-                    result.append([nums[i], nums[j],nums[k]])
-                    j += 1
-                    k -= 1
-                    while j < k and nums[j] == nums[j-1]:
-                        j += 1
-                    while j < k and nums[k] == nums[k+1]:
-                        k -= 1
-        return result
+                    psum_store.add(nums[j])
+        return list(tripStore)
+        # st = set()
+
+        # for i in range(n):
+        #     hashset = set()
+        #     for j in range(i + 1, n):
+        #         # Calculate the 3rd element:
+        #         third = -(arr[i] + arr[j])
+
+        #         # Find the element in the set:
+        #         if third in hashset:
+        #             temp = [arr[i], arr[j], third]
+        #             temp.sort()
+        #             st.add(tuple(temp))
+        #         hashset.add(arr[j])
+
+        # # store the set in the answer:
+        # ans = list(st)
+        # return ans
