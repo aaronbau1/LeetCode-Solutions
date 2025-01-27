4991f0395a324918af74ead3f3da366a30baf841
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-27 12:57:18 -0600

    Time: 56 ms (62.85%), Space: 32.8 MB (17.52%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,17 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # brute find all subarrays and calculate sums
-        # better: prefix sum and calculate greatest value from the hashmap
-        # optimal: two pointers and start over when it is lower than zero
-        local_sum = 0
-        max_sum = float("-inf")
-
+        total = float("-inf")
+        psum = 0
         for i in range(len(nums)):
-            local_sum += nums[i]
-            max_sum = max(max_sum, local_sum)
-            if local_sum < 0:
-                local_sum = 0
-        return max_sum
+            psum += nums[i]
+            total = max(psum, total)
 
-            
-
+            if psum < 0:
+                psum = 0
+        return total
