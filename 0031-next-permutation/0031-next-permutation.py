877bafe6b844ba85d4062f3702408e8dd10efd90
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-27 13:15:55 -0600

    Time: 0 ms (100%), Space: 17.6 MB (57.63%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -5,21 +5,20 @@ class Solution:
         """
         pivot = -1
         for i in range(len(nums)-1, 0, -1):
-            if nums[i-1] < nums[i]:
+            if nums[i] > nums[i-1]:
                 pivot = i - 1
                 break
-        # print(pivot)
+        
         if pivot == -1:
-            nums[:] = nums[::-1]
+            nums[:] = nums[:][::-1]
             return
 
+        # find first num higher than pivot
         for i in range(len(nums)-1, pivot, -1):
             if nums[i] > nums[pivot]:
-                nums[i], nums[pivot] = nums[pivot], nums[i]
+                nums[pivot], nums[i] = nums[i], nums[pivot]
                 break
-        # print(nums)
+        
+        # reverse rest of array
         nums[pivot+1:] = nums[pivot+1:][::-1]
-
-
-
         
