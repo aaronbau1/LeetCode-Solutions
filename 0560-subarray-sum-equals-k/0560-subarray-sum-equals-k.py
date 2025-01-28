3792193cd7732304ae053ad257c106a425a4d902
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-28 08:48:05 -0600

    Time: 24 ms (90.91%), Space: 20.5 MB (29.31%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,8 +1,8 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         store = {0:1}
-        count = 0
         psum = 0
+        count = 0
 
         for i in range(len(nums)):
             psum += nums[i]
@@ -10,10 +10,10 @@ class Solution:
 
             if rem in store:
                 count += store[rem]
-            
-            # means a subarray summed to zero, incr count
+
             if psum in store:
                 store[psum] += 1
             else:
                 store[psum] = 1
+
         return count
