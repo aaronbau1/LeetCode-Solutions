7666b960bf0b7e2899b01bd69dc13aea7028aeaa
Aaron Baumgarten <112639833+aaronbau1@users.noreply.github.com>
2025-01-30 16:57:44 -0600

    Time: 0 ms (100%), Space: 18.4 MB (63.68%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,31 +3,29 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
+        first_col = False
         rows = len(matrix)
         cols = len(matrix[0])
-        first_col = False
-        
+
         for i in range(rows):
             for j in range(cols):
                 if matrix[i][j] == 0:
                     matrix[i][0] = 0
-                    if j == 0:
-                        first_col = True
-                    else:
+                    if j != 0:
                         matrix[0][j] = 0
+                    else:
+                        first_col = True
         
-        for i in range(1, rows):
+        for i in range(1,rows):
             for j in range(1, cols):
-                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
         
-        # check first row and first col
+        # check first row
         if matrix[0][0] == 0:
-            for j in range(cols):
-                matrix[0][j] = 0
-                
-        if first_col == True:
-            for i in range(rows):
-                matrix[i][0] = 0
+            for i in range(cols):
+                matrix[0][i] = 0
         
+        if first_col == True:
+            for j in range(rows):
+                matrix[j][0] = 0
